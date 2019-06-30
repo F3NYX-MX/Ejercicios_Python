ae2695d5d218c79a9a8604333b7575e2e808c2f4
F3NYX-MX <lemuri.colli@gmail.com>
2019-06-30 01:40:06 -0500

    finalizado primer curso basico de python:

--- 11_modules.py
+++ 11_modules.py
@@ -2,6 +2,13 @@
 # thirdy party modules
 # python modules
 
+# tambien se pueden importa de manera parcial solo los metodos que necesitemos.
+
 import datetime
+from matematicas import sumar, resta
 
 print(datetime.date.today())
+
+sumar(89, 9)
+resta(89,78)
+
---
+++ matematicas.py
@@ -0,0 +1,13 @@
+#modulo con las funciones basicas de + - * /
+
+def sumar(n1, n2):
+    print(n1 + n2)
+
+def resta(n1,n2):  
+    print(n1 - n2)
+
+def multiplica(n1, n2):
+    print (n1 * n2)
+
+def divide(n1, n2):
+    print(n1 / n2)
